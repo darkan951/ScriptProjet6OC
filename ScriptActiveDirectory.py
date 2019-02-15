9051a92b5ade12452538b31a7526c4de93e12b76
darkan951 <47302016+darkan951@users.noreply.github.com>
2019-02-15 13:02:58 +0100

    Create ScriptActiveDirectory.py
    Ajout fonction de connexion

---
+++ ScriptActiveDirectory.py
@@ -0,0 +1,39 @@
+import ldap
+import sys
+
+# Création du dictionnaire pour stocker les informations de connexion à l'Active Directory
+
+LDAP = {
+    """adresse_ad""" : """ldaps://srvad.projet6.oc:636""" ,
+    """base_ad""" : """DC=projet6,DC=oc""" ,
+    """admin_ad""" : """""" ,
+    """passw_ad""" : """ZAR_&"kan"""
+}
+
+
+# Fonction de connexion à l'Active Directory en tant qu'admin du domaine
+
+def connexionAD():
+
+    # connexion à l'AD et création de l'objet AD
+    print("initialisation")
+    try:
+        ldap.set_option(ldap.OPT_X_TLS_REQUIRE_CERT, ldap.OPT_X_TLS_NEVER)
+        AD = ldap.initialize(LDAP["""adresse_ad"""])
+    except ldap.LDAPError, e:
+        print e
+        sys.exit(1)
+    
+    # définition du protocol
+    AD.protocol_version = ldap.VERSION3
+
+    # connexion de l'admin du domaine
+    print("connexion")
+    try:
+        AD.bind_s(LDAP["""admin_ad"""], LDAP["""passw_ad"""])
+    except ldap.LDAPError, e:
+        print e
+        sys.exit(1)
+
+
+AD.unbind()
