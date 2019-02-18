55b2a85370de19c33360f10c313a72b8737f47b6
darkan951 <47302016+darkan951@users.noreply.github.com>
2019-02-18 17:50:40 +0100

    Update ScriptActiveDirectory.py
    legere modif de la fonction de connexion

--- ScriptActiveDirectory.py
+++ ScriptActiveDirectory.py
@@ -1,9 +1,10 @@
 import ldap
 import sys
+from time import sleep
 
 # Création du dictionnaire pour stocker les informations de connexion à l'Active Directory
 
-LDAP = {
+LDAP_dict = {
     """adresse_ad""" : """ldaps://srvad.projet6.oc:636""" ,
     """base_ad""" : """DC=projet6,DC=oc""" ,
     """admin_ad""" : """""" ,
@@ -19,7 +20,7 @@ def connexionAD():
     print("initialisation")
     try:
         ldap.set_option(ldap.OPT_X_TLS_REQUIRE_CERT, ldap.OPT_X_TLS_NEVER)
-        AD = ldap.initialize(LDAP["""adresse_ad"""])
+        AD = ldap.initialize(LDAP_dict["""adresse_ad"""])
     except ldap.LDAPError, e:
         print e
         sys.exit(1)
@@ -30,12 +31,18 @@ def connexionAD():
     # connexion de l'admin du domaine
     print("connexion")
     try:
-        AD.bind_s(LDAP["""admin_ad"""], LDAP["""passw_ad"""])
+        AD.bind_s(LDAP_dict["""admin_ad"""], LDAP_dict["""passw_ad"""])
     except ldap.LDAPError, e:
         print e
         sys.exit(1)
     
     return AD
+
+
+# Fonction d'ajout d'utilisateur à l'AD
+
+def ajoutUtil( nomU, prenomU, passordU ): # nom et prénom de l'utilisateur et mot de passe pour premiere connexion
+
 
 
 AD.unbind()
