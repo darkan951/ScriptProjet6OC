d4e5a6af645d73615e3048dce31b51922975f6a2
darkan951 <47302016+darkan951@users.noreply.github.com>
2019-02-19 19:19:25 +0100

    Update ScriptManageAD.py
    connexion ok

--- ScriptManageAD.py
+++ ScriptManageAD.py
@@ -1,16 +1,20 @@
 import ldap3
 import time
+import sys
 
 # definition du serveur
 srvAD = ldap3.Server('srvAD.projet6.oc')
 
 try:
     # création de l'objet contenant l'AD et sur lequel s'éffecturont les modifications
-    ObjetAD = ldap3.Connection(srvAD, user='cn=franck hebert,ou=ServiceTechnique,ou=SocieteX,dc=projet6,dc=oc', password='ZAR_&"kan')
+    ObjetAD = ldap3.Connection(srvAD, user='cn=franck hebert,ou=ServiceTechnique,ou=SocieteX,dc=projet6,dc=oc', password='ZAR_&"kan', auto_bind=True)
     print("connecté")
     time.sleep(5)
-except ldap3.core.exceptions.LDAPBindError as e:
-    print("echec", e)
+except ldap3.core.exceptions.LDAPException as e:
+    print("echec connexion :", e)
     time.sleep(5)
 
+choix_util = input("1 - Ajouter un utilisateur \n2 - Modifier un utilisateur")
 
+
+ObjetAD.unbind()
