49d6b816037f08635d1875d80ba1a569fa58f1fa
darkan951 <47302016+darkan951@users.noreply.github.com>
2019-02-21 14:37:43 +0100

    Update ScriptPythonLdap.py
    création de la fonction main

--- ScriptPythonLdap.py
+++ ScriptPythonLdap.py
@@ -5,8 +5,9 @@ import sys
 # definition du dictionnaire contenant les informations de connexion
 dictLDAP = {
     'srvAD':'srvAD.projet6.oc',
-    'admin':'cn=franck hebert,ou=ServiceTechnique,ou=SocieteX,dc=projet6,dc=oc',
-    'mdp':'ZAR_&"kan'
+    'admin':'cn=Franck Hebert,ou=ServiceTechnique,ou=SocieteX,dc=projet6,dc=oc',
+    'mdp':'ZAR_&"kan',
+    'base':'dc=projet6,dc=oc'
 }
 
 # définition de la fonction de connexion
@@ -17,6 +18,15 @@ def Connexion (dictLDAP) :
         print(connex)
     except ldap.LDAPError as e:
         print("echec connexion :", e)
+        sys.exit(1)
 
-        
-Connexion(dictLDAP)
+
+# definition de la fonction d'ajout d'utilisateur
+# def AjoutUtil () :
+
+
+# definition du main qui servira à gerer le déroulé du script        
+def main():
+    Connexion(dictLDAP)
+
+main()
